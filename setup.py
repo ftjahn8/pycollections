2abbf6677a74f750a4f42bb9ddb85a0f5045c4bc
felix.etzkorn@hm.edu <felix.etzkorn@web.de>
2022-07-05 00:32:47 +0200

    fix typos

--- setup.py
+++ setup.py
@@ -19,16 +19,16 @@ setup(
     packages=find_packages(),
     install_requires=requirements,
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha"
-        "Intended Audience :: Developers"
-        "Natural Language :: English"
-        "Programming Language :: Python"
-        "Programming Language :: Python :: 3"
-        "Programming Language :: Python :: 3 :: Only"
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10"
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
